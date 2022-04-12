4d648fe70c34ca47eb6d246693f99eb33cff9a7c
Jamesstahl98 <Jamesstahl556@gmail.com>
2022-04-12 16:36:47 +0200

    magma + kommentera bort camera controller

--- Spelprototyp 3/Assets/Scripts/FireCollider.cs
+++ Spelprototyp 3/Assets/Scripts/FireCollider.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class FireCollider : MonoBehaviour
 {
+    public GameObject magmaPrefab;
+
+    public float magmaForce = 2f;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
@@ -13,12 +17,20 @@ public class FireCollider : MonoBehaviour
 
         if (collision.gameObject.tag == "Earth")
         {
-            
+            MagmaSpawn();
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
 
         if (collision.gameObject.tag == "Wind")
         {
             
         }
+    }
+    void MagmaSpawn()
+    {
+        GameObject magmaCircle = Instantiate(magmaPrefab, transform.position, transform.rotation);
+        Rigidbody2D rb = magmaCircle.GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * magmaForce, ForceMode2D.Impulse);
     }
 }
