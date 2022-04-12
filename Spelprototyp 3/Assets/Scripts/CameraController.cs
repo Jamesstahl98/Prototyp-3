9af6ee1c2639bc5a3b5f9c09895b634c8c027392
ArthurJ01 <arthur.jerlstrom@gmail.com>
2022-04-12 16:08:15 +0200

    Wapow

--- Spelprototyp 3/Assets/Scripts/CameraController.cs
+++ Spelprototyp 3/Assets/Scripts/CameraController.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public Vector3 offset;
+    [Range(2, 10)]
+    public float smoothFactor;
 
-    // Start is called before the first frame update
-    void Start()
+    private void FixedUpdate()
     {
-        
+        Follow();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Follow()
     {
-        //transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.)
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = smoothPosition;
     }
 }
