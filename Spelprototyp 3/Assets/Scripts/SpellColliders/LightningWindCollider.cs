3e9568fc3dcf91682a320d387b18995d76951978
Jamesstahl98 <Jamesstahl556@gmail.com>
2022-04-13 02:11:32 +0200

    Spell Colliders

---
+++ Spelprototyp 3/Assets/Scripts/SpellColliders/LightningWindCollider.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningWindCollider : MonoBehaviour
+{
+    public float baseDamage;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //collision.gameObject.GetComponent<Enemy>().TakeDamage(baseDamage);
+        }
+    }
+}
