b06c07257ffb18a9c69d83b5ee1eadbe313f22b7
Jamesstahl98 <Jamesstahl556@gmail.com>
2022-04-14 17:58:14 +0200

    Plantans ai

---
+++ Spelprototyp 3/Assets/Scripts/SpellColliders/PlantProjectileCollider.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantProjectileCollider : MonoBehaviour
+{
+    public float baseDamage;
+    public float spellHP;
+
+    public string damageType;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyHit>().TakeDamage(baseDamage, damageType);
+            spellHP -= 1;
+
+            if (spellHP <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Spelprototyp 3/Assets/Scripts/SpellControllers/PlantController.cs
+++ Spelprototyp 3/Assets/Scripts/SpellControllers/PlantController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlantController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject projectilePrefab;
+    public Transform firePoint;
 
-    // Update is called once per frame
+    public float projectileForce;
+    public float spellCD;
+
+    private float spellTimer = 2f;
+
     void Update()
     {
-        
+        spellTimer += Time.deltaTime;
+        if(spellTimer >= spellCD)
+        {
+            FireAtClosestEnemy();
+        }
+    }
+
+    void FireAtClosestEnemy()
+    {
+        float distanceToClosestEnemy = Mathf.Infinity;
+        Enemy closestEnemy = null;
+        Enemy[] allEnemies = GameObject.FindObjectsOfType<Enemy>();
+
+        foreach(Enemy currentEnemy in allEnemies)
+        {
+            float distanceToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+            if(distanceToEnemy < distanceToClosestEnemy)
+            {
+                distanceToClosestEnemy = distanceToEnemy;
+                closestEnemy = currentEnemy;
+            }
+        }
+        if (transform.position.x > closestEnemy.transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+        rb.AddForce((closestEnemy.transform.position-transform.position) * projectileForce, ForceMode2D.Impulse);
+        spellTimer = 0;
     }
 }
