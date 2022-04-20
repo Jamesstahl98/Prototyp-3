3a84f63bbeb762f6755ce4973dd6b6f8e94f10ec
ArthurJ01 <arthur.jerlstrom@gmail.com>
2022-04-20 21:42:18 +0200

    Level System fix

---
+++ Spelprototyp 3/Assets/Scripts/LevelStuff/Test/EquipWindow.cs
@@ -0,0 +1,32 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class EquipWindow : MonoBehaviour {
+
+  //  [SerializeField] private Player player;
+    [SerializeField] private Sprite headSprite;
+    [SerializeField] private Sprite helmet1Sprite;
+    [SerializeField] private Sprite helmet2Sprite;
+
+    private LevelSystem levelSystem;
+
+   
+    public void SetLevelSystem(LevelSystem levelSystem) {
+        this.levelSystem = levelSystem;
+    }
+
+}
