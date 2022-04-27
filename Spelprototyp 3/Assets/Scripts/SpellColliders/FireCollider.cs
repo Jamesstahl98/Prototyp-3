4214fa169c3581786fc9070a10b376a9a07d40d3
Jamesstahl98 <Jamesstahl556@gmail.com>
2022-04-27 17:28:28 +0100

    Fire light fades out

--- Spelprototyp 3/Assets/Scripts/SpellColliders/FireCollider.cs
+++ Spelprototyp 3/Assets/Scripts/SpellColliders/FireCollider.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class FireCollider : MonoBehaviour
 {
@@ -10,16 +11,32 @@ public class FireCollider : MonoBehaviour
     public float magmaForce = 2f;
     private float baseDamage;
     private int spellHP;
+    private bool spellDead = false;
 
     public string damageType;
 
     private GameObject player;
+    private ParticleSystem ps;
+    private SpriteRenderer sr;
+    private BoxCollider2D collider;
 
     void Start()
     {
         player = GameObject.Find("Player");
+        ps = gameObject.GetComponent<ParticleSystem>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        collider = gameObject.GetComponent<BoxCollider2D>();
         baseDamage = player.GetComponent<Shooting>().fireDamage;
         spellHP = player.GetComponent<Shooting>().fireHP;
+    }
+
+    void FixedUpdate()
+    {
+        if (spellDead == true && gameObject.GetComponent<Light2D>().intensity >= 0)
+        {
+            Debug.Log("spell dead");
+            gameObject.GetComponent<Light2D>().intensity = gameObject.GetComponent<Light2D>().intensity - 0.02f;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -31,7 +48,10 @@ public class FireCollider : MonoBehaviour
 
             if (spellHP <= 0)
             {
-                Destroy(gameObject);
+                ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+                sr.GetComponent<Renderer>().enabled = false;
+                collider.enabled = false;
+                StartCoroutine(PauseCode());
             }
         }
 
@@ -60,5 +80,17 @@ public class FireCollider : MonoBehaviour
     void FireTornadoSpawn()
     {
         GameObject fireCircle = Instantiate(fireTornadoPrefab, transform.position, transform.rotation);
+    }
+
+    IEnumerator PauseCode()
+    {
+        ReduceIntensity();
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+
+    void ReduceIntensity()
+    {
+        spellDead = true;
     }
 }
